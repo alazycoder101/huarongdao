d4f6d2deb49c17975b7d2726e1e638b3dc9f68cd
Lazy coder <82757613+alazycoder101@users.noreply.github.com>
2024-09-08 23:03:55 +0100

    try to manually import the package for vue.5.3

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Unocss from 'unocss/vite'
+import ReactivityTransform from '@vue-macros/reactivity-transform/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({ reactivityTransform: true }), vueJsx(), Unocss()],
+  plugins: [vue(), vueJsx(), Unocss(), ReactivityTransform()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
